ede44bc3e31ac232e69cf4e7c0e0aa7c5fa0854d
Leejjon <leejjon@gmail.com>
2024-12-18 16:15:31 +0100

    Still can't reproduce.

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,81 @@
 import { Autocomplete, Box, TextField } from '@mui/material';
 import './App.css';
-import { SyntheticEvent, useState } from 'react';
+import { ChangeEvent, useState } from 'react';
+
+export interface Match {
+  id: string
+  startTimestamp: string,
+  competitionName: string,
+  homeTeamName: string,
+  homeTeamID: string,
+  awayTeamName: string,
+  awayTeamID: string,
+  finished: boolean,
+  score: Score
+}
+
+export interface Score {
+  home: number,
+  away: number
+}
 
 function App() {
   const [inputValue, setInputValue] = useState<string>('');
-  const [selectedMatch, setSelectedMatch] = useState<string | undefined>(undefined);
-  const [matches] = useState(["AZ - Ajax", "PSV - Feyenoord"]);
+  const [selectedMatchId, setSelectedMatchId] = useState<string>();
+  const [matches] = useState([
+    {score:{away:0,home:0},competitionId:"2001",competitionName:"Champions League",awayTeamName:"Sturm Graz",finished:false,homeTeamName:"Atalanta",homeTeamID:"102",awayTeamID:"2021",startTimestamp:"2025-01-21T17:45:00.000Z",id:"football-data-523975"},
+    {score:{away:0,home:0},competitionId:"2001",competitionName:"Champions League",awayTeamName:"Aston Villa",finished:false,homeTeamName:"AS Monaco FC",homeTeamID:"548",awayTeamID:"58",startTimestamp:"2025-01-21T17:45:00.000Z",id:"football-data-524043"},
+]);
   // The fuck is wrong with this: https://github.com/mui/material-ui/issues/29943
+
+  const getHomeTeamNameToDisplay = (match: Match) => {
+    if (match.competitionName === 'European Cup' && window.location.hostname.endsWith('blindepool.nl')) {
+        return parseInt(match.homeTeamID);
+    } else {
+        return match.homeTeamName;
+    }
+  }
+
+  const getAwayTeamNameToDisplay = (match: Match) => {
+    if (match.competitionName === 'European Cup' && window.location.hostname.endsWith('blindepool.nl')) {
+        return parseInt(match.awayTeamID);
+    } else {
+        return match.awayTeamName;
+    }
+  }
+
+  const displayMatchInDropdown = (upcomingMatch: Match | string): string => {
+    const homeTeamName = getHomeTeamNameToDisplay(upcomingMatch as Match);
+    const awayTeamName = getAwayTeamNameToDisplay(upcomingMatch as Match);
+    return `${homeTeamName} vs ${awayTeamName}`;
+  };
+
+  const updateSelectMatch = (_event: ChangeEvent<object> | null, selectedMatch: null | string | Match) => {
+    const supportedMatch = selectedMatch as Match;
+    if (supportedMatch && supportedMatch.id) {
+      setSelectedMatchId(supportedMatch.id);
+    }
+  };
+
   return (
     <>
       <Autocomplete
-      disablePortal
+      disabled={matches.length < 1}
       freeSolo
+      onChange={updateSelectMatch}
       inputValue={inputValue}
-      onInputChange={(_event: SyntheticEvent, value: string) => {
-          setInputValue(value)
+      onInputChange={(_event: ChangeEvent<{}>, newSupportedMatch: string) => {
+          setInputValue(newSupportedMatch)
       }}
-      onChange={(_event: SyntheticEvent, value: string | null) => {
-        if (value) {
-          setSelectedMatch(value);
-        }
-      }}
-      options={matches}
+      getOptionLabel={displayMatchInDropdown}
+      options={matches as Match[]}
       sx={{ width: 300 }}
-      renderOption={(props: any, option: string) => {
-        return (<Box {...props} key={option}>{option}<br/></Box>);
+      renderOption={(props: any, option: Match) => {
+        return (<li {...props} key={option.id}>{option.id}<br/></li>);
       }}
       renderInput={(params) => <TextField {...params} label="Match" />}
       />
-      <p>Selected match: {selectedMatch}</p>
+      <p>Selected match: {selectedMatchId}</p>
     </>
   )
 }
